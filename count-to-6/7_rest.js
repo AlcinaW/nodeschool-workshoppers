c70c2ebc3ce64b32b30040b116256f20c0d01fcb
Alcina <white_kaitou@hotmail.com>
2017-07-26 23:46:07 -0400

    solution added

--- count-to-6/7_rest.js
+++ count-to-6/7_rest.js
@@ -1,3 +1,9 @@
-module.exports = function average(/* what goes here */) {
-    // what goes here?
+module.exports = function average(...args) {
+    return args.reduce((sum, x) => sum + x, 0) / args.length;
 };
+
+
+// module.exports = (...args) => {
+//      var sum = args.reduce((soFar, value) => soFar + value, 0);
+//      return sum / args.length;
+//  };
